c02bb0a5057a5ee81582353e49a50d5bafb3b6ba
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2022-06-02 17:31:33 +0300

    Update PostsDetail.vue

--- src/components/posts/PostsDetail.vue
+++ src/components/posts/PostsDetail.vue
@@ -2,22 +2,10 @@
   <section>
     <div class="posts-item">
       <div class="posts-item__content">
-        <p class="posts-item__id">
-          ID:
-          {{ details.id }}
-        </p>
-        <p class="posts-item__userId">
-          UserID:
-          {{ details.userId }}
-        </p>
-        <p class="posts-item__title">
-          Title:
-          {{ details.title }}
-        </p>
-        <p class="posts-item__body">
-          Body:
-          {{ details.body }}
-        </p>
+        <p class="posts-item__id">ID:{{ details.id }}</p>
+        <p class="posts-item__userId">UserID:{{ details.userId }}</p>
+        <p class="posts-item__title">Title:{{ details.title }}</p>
+        <p class="posts-item__body">Body:{{ details.body }}</p>
       </div>
     </div>
   </section>
@@ -35,4 +23,8 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.posts-item__userId {
+  cursor: pointer;
+}
+</style>
