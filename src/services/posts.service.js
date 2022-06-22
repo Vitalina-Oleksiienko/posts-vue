b22d663244d0a72916366c2f412d154fb6f5af51
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2022-06-22 20:02:31 +0300

    finish

--- src/services/posts.service.js
+++ src/services/posts.service.js
@@ -1,9 +1,5 @@
 import axios from '../utils/axios';
 
-// export const getPostsList = () => {
-//     return axios.get(`/posts`);
-// };
-
 export const getPostsById = (id) => {
     return axios.get(`/posts/${id}`);
 };
