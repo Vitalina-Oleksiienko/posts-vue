d40a1ef5901799ed3269a5ab70cc5d5027a1b093
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2022-06-23 17:42:25 +0300

    Update MainContainer.vue

--- src/components/shared/MainContainer.vue
+++ src/components/shared/MainContainer.vue
@@ -13,26 +13,22 @@ export default {
 <style lang="scss" scoped>
 .container {
   max-width: 1190px;
-
   margin: 0 auto;
-  //padding: 0 10px;
+  padding: 0 10px;
 }
 @media screen and (max-width: 1199.98px) {
   .container {
     max-width: 100%;
-    padding: 0 10px;
   }
 }
 @media screen and (max-width: 767.98px) {
   .container {
     max-width: 100%;
-    margin: 0 auto;
   }
 }
 @media screen and (max-width: 419.98px) {
   .container {
     min-width: 400px;
-    margin: 0 auto;
   }
 }
 </style>
